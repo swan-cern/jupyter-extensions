3810dfabb66a22bbf8454f7fe541d0b6eb706dbb
Diogo Castro <diogo.castro@cern.ch>
2017-11-02 17:17:47 +0100

    Swan notifications frontend extension

---
+++ SwanNotifications/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='swannotifications',
+      version='0.0.1',
+      description='Notifications for SWAN',
+      include_package_data=True,
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+      ],
+  )
