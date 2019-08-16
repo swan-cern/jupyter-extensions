66bf6ed3ad709d792db7a8d667aedb09eb95cf71
Sahil Jajodia <sahil.jajodia@gmail.com>
2019-08-16 14:56:24 +0530

    Fix create-user bug

--- K8sSelection/k8sselection/kernelextension.py
+++ K8sSelection/k8sselection/kernelextension.py
@@ -569,8 +569,8 @@ class K8sSelection:
 
                 # Deploy helm chart for user
                 my_env = os.environ.copy()
-                command = ["helm", "install", "--name", "spark-user-" + os.environ['USER'], "--set",
-                           "user.name=" + os.environ['USER'], "--set", "cvmfs.enable=true", "--set", "user.admin=false",
+                command = ["helm", "install", "--name", "spark-user-" + username, "--set",
+                           "user.name=" + username, "--set", "cvmfs.enable=true", "--set", "user.admin=false",
                            "https://gitlab.cern.ch/db/spark-service/spark-service-charts/raw/spark_user_accounts/cern-spark-user-1.1.0.tgz"]
                 p = subprocess.Popen(command, stdout=subprocess.PIPE, env=my_env)
                 out, err = p.communicate()
