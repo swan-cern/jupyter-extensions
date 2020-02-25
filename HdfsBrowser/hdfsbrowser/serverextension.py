ee9f9f7365eb9a3d31bb7acb7643cffe3833e39c
Piotr Mrowczynski <piotr.mrowczynski@cern.ch>
2020-02-25 15:44:04 +0100

    properly format strings in hdfsbrowser

--- HdfsBrowser/hdfsbrowser/serverextension.py
+++ HdfsBrowser/hdfsbrowser/serverextension.py
@@ -30,13 +30,15 @@ class HDFSBrowserConfig():
         elif cluster == 'hadoop-nxcals':
             self.hdfs_site_namenodes_property = 'dfs.ha.namenodes.nxcals'
         else:
-            self.hdfs_site_namenodes_property = 'dfs.ha.namenodes.' + cluster
-        self.hdfs_site_path = '/cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/conf/etc/' + cluster + '/hadoop.' \
-               + cluster + '/hdfs-site.xml'
+            self.hdfs_site_namenodes_property = 'dfs.ha.namenodes.{0}'.format(
+                cluster)
+
+        self.hdfs_site_path = '/cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/conf/etc/{0}/hadoop.{0}/hdfs-site.xml'.format(
+            cluster)
         self.hdfs_site_namenodes_port = 50070
 
         # define webhdfs token
-        self.webhdfs_token = os.environ.get("WEBHDFS_TOKEN", "")
+        self.webhdfs_token = os.environ.get('WEBHDFS_TOKEN', '')
 
         # Do not respond to webhdfs requests longer than this value e.g. for download
         self.webhdfs_response_timeout = 3600
@@ -80,8 +82,8 @@ class HDFSBrowserProxy(IPythonHandler):
 
         # get active namenode
         for namenode in namenodes.split(','):
-            nmd_active_url = 'http://' + namenode + ':' + str(self.hdfs_browser_config.hdfs_site_namenodes_port) + \
-                                    '/jmx?get=Hadoop:service=NameNode,name=NameNodeStatus::State'
+            nmd_active_url = 'http://{0}:{1}/jmx?get=Hadoop:service=NameNode,name=NameNodeStatus::State'.format(
+                namenode, self.hdfs_browser_config.hdfs_site_namenodes_port)
 
             try:
                 response = yield AsyncHTTPClient().fetch(
@@ -89,19 +91,20 @@ class HDFSBrowserProxy(IPythonHandler):
                     raise_error=False
                 )
             except Exception:
-                self.log.error('NameNode request ' + nmd_active_url + ' failed, state could not be retrieved')
+                self.log.error('NameNode request {0} failed, state could not be retrieved'.format(nmd_active_url))
                 self.log.error(traceback.format_exc())
                 break
 
             if response and response.body:
                 namenode_state = json.loads(response.body)['beans'][0]['State']
-                self.log.debug('NameNode ' + namenode + ' state is ' + namenode_state)
+                self.log.debug('NameNode {0} state is {1}'.format(namenode, namenode_state))
 
                 if namenode_state == 'active':
-                    self.active_namenode_url = 'http://' + namenode + ':' + str(self.hdfs_browser_config.hdfs_site_namenodes_port)
+                    self.active_namenode_url = 'http://{0}:{1}'.format(
+                        namenode, self.hdfs_browser_config.hdfs_site_namenodes_port)
                     break
             else:
-                self.log.error('NameNode request ' + nmd_active_url + ' responded with empty response')
+                self.log.error('NameNode request {0} responded with empty response').format(nmd_active_url)
 
     @gen.coroutine
     def get(self):
@@ -120,7 +123,7 @@ class HDFSBrowserProxy(IPythonHandler):
         request_path = \
             self.request.uri[self.request.uri.index(self.proxy_root)
                              + len(self.proxy_root) + 1:]
-        self.log.debug("HDFSBrowserHandler handle request " + request_path)
+        self.log.debug('HDFSBrowserHandler handle request {}'.format(request_path))
 
         if not self.active_namenode_url:
             raise web.HTTPError(status_code=500, log_message='HDFS Browser not available, no active hdfs namenode')
@@ -128,10 +131,10 @@ class HDFSBrowserProxy(IPythonHandler):
         # proxy the request
         hdfs_browser_url = url_path_join(self.active_namenode_url, request_path)
 
-        self.log.debug("HDFSBrowserHandler proxing request " + hdfs_browser_url)
+        self.log.debug('HDFSBrowserHandler proxing request {}'.format(hdfs_browser_url))
 
         try:
-            if "/webhdfs/v1" in hdfs_browser_url:
+            if '/webhdfs/v1' in hdfs_browser_url:
                 yield AsyncHTTPClient(force_instance=True,
                                       max_body_size=self.hdfs_browser_config.webhdfs_max_body_size,
                                       max_buffer_size=self.hdfs_browser_config.webhdfs_max_chunk_size).fetch(
@@ -155,18 +158,19 @@ class HDFSBrowserProxy(IPythonHandler):
                 self.handle_explorer_response(explorer_response)
         except Exception:
             self.log.error(traceback.format_exc())
-            raise web.HTTPError(status_code=500, log_message='HDFS Browser request ' + hdfs_browser_url + ' failed')
+            raise web.HTTPError(status_code=500, log_message='HDFS Browser request {0} failed'.format(hdfs_browser_url))
 
     def handle_explorer_response(self, response):
         content_type = response.headers['Content-Type']
 
-        self.log.debug("HDFSBrowserHandler explorer " + self.request.uri + " response content-type: " + content_type)
+        self.log.debug('HDFSBrowserHandler explorer {0} response content-type {1}'.format(
+            self.request.uri, content_type))
 
         replace_path = self.request.uri[:self.request.uri.index(self.proxy_root) + len(self.proxy_root)]
 
         if 'text/html' in content_type:
             # a regular expression to match paths against the Spark on EMR proxy paths
-            PROXY_PATH_RE = re.compile(r"(.*)")
+            PROXY_PATH_RE = re.compile(r'(.*)')
 
             # a tuple of tuples with tag names and their attribute to automatically fix
             PROXY_ATTRIBUTES = ((('a', 'link'), 'href'), (('img', 'script'), 'src'))
@@ -181,7 +185,8 @@ class HDFSBrowserProxy(IPythonHandler):
                     if match is not None:
                         value = match.groups()[0]
                     tag[attribute] = url_path_join(replace_path, value)
-                    self.log.debug("REPLACE: tag_attribute_old: %s tag_attribute_new: %s", value, tag[attribute])
+                    self.log.debug('REPLACE: tag_attribute_old: {0} tag_attribute_new: {1}'.format(
+                        value, tag[attribute]))
 
             content = soup.prettify()
         elif 'javascript' in content_type:
@@ -189,10 +194,9 @@ class HDFSBrowserProxy(IPythonHandler):
                 '/webhdfs/v1',
                 url_path_join(replace_path, '/webhdfs/v1')
             )
-            token = 'delegation=' + self.hdfs_browser_config.webhdfs_token
             content = content.replace(
                 '?op=',
-                '?' + token.strip() + '&op='
+                '?delegation={0}&op='.format(self.hdfs_browser_config.webhdfs_token.strip())
             )
         else:
             content = response.body
@@ -211,9 +215,9 @@ class HDFSBrowserProxy(IPythonHandler):
         if not header_line:
             return
 
-        self.log.debug("HDFSBrowserHandler webhdfs " + header_line + " header")
+        self.log.debug('HDFSBrowserHandler webhdfs {0} header'.format(header_line))
 
-        if header_line.startswith("HTTP/"):
+        if header_line.startswith('HTTP/'):
             try:
                 status = httputil.parse_response_start_line(header_line)
                 self.set_status(status.code)
@@ -233,7 +237,7 @@ class HDFSBrowserProxy(IPythonHandler):
         """Handles response chunk of the data by writing it onto the network
         """
 
-        self.log.debug("HDFSBrowserHandler webhdfs " + self.request.uri + " chunk")
+        self.log.debug('HDFSBrowserHandler webhdfs {0} chunk'.format(self.request.uri))
 
         self.write(chunk)
         self.flush()
@@ -241,7 +245,7 @@ class HDFSBrowserProxy(IPythonHandler):
     def handle_webhdfs_stream_finish(self):
         """Handles finish of the response
         """
-        self.log.debug("HDFSBrowserHandler webhdfs " + self.request.uri + " response finish")
+        self.log.debug('HDFSBrowserHandler webhdfs {0} finish'.format(self.request.uri))
         self.finish()
 
     def compute_etag(self):
@@ -253,11 +257,11 @@ def load_jupyter_server_extension(nbapp):
     """Load the Jupyter server extension.
     """
 
-    nbapp.log.info("HDFS Browser enabled")
-    base_url = nbapp.web_app.settings["base_url"]
+    nbapp.log.info('HDFS Browser enabled')
+    base_url = nbapp.web_app.settings['base_url']
     hdfs_browser_proxy_root = '/hdfsbrowser'
     hdfs_browser_endpoint = url_path_join(base_url, hdfs_browser_proxy_root)
     hadoop_handlers = [
-        (hdfs_browser_endpoint + ".*", HDFSBrowserProxy, dict(proxy_root=hdfs_browser_proxy_root))
+        ('{0}.*'.format(hdfs_browser_endpoint), HDFSBrowserProxy, dict(proxy_root=hdfs_browser_proxy_root))
     ]
-    nbapp.web_app.add_handlers(".*", hadoop_handlers)
+    nbapp.web_app.add_handlers('.*', hadoop_handlers)
