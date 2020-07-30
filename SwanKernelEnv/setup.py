4ac25668c535fa313576157863fda30681c24bad
Diogo Castro <diogo.castro@cern.ch>
2020-07-30 22:56:48 +0200

    SwanKernelEnv: pip installable

--- SwanKernelEnv/setup.py
+++ SwanKernelEnv/setup.py
@@ -1,14 +1,48 @@
-#!/usr/bin/env python
+"""
+Setup Module to setup Python Handlers for the SwanKernelEnv extension.
+"""
+import os
 
-from setuptools import setup, find_packages
+from jupyter_packaging import get_version
+import setuptools
 
-setup(name='swankernelenv',
-      version='0.0.1',
-      description='Kernel extension to manipulate the Python path of the user',
-      include_package_data=True,
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=[
-          'notebook'
-      ],
-  )
+name="swankernelenv"
+
+# Get our version
+version = get_version(os.path.join(name, "_version.py"))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name=name,
+    version=version,
+    url="https://github.com/swan-cern/jupyter-extensions",
+    author="SWAN Admins",
+    description="Ipython kernel extension to remove SWAN extra configurations from the user environment",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "ipykernel",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    license="AGPL-3.0",
+    platforms="Linux, Mac OS X, Windows",
+    keywords=["Jupyter", "Notebooks", "SWAN", "CERN"],
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Jupyter",
+    ],
+)
+
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_args)
