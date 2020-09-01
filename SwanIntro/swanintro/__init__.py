1d1c718fe1dd61eae04794bdc3ce57b6c2b80081
Diogo Castro <diogo.castro@cern.ch>
2020-09-01 23:08:26 +0200

    SwanIntro: pip installable

--- SwanIntro/swanintro/__init__.py
+++ SwanIntro/swanintro/__init__.py
@@ -1,9 +1,11 @@
+from ._version import __version__ 
+
 
 def _jupyter_nbextension_paths():
     # Used by "jupyter nbextension" command to install frontend extension
     return [dict(
                 section="tree",
-                src="js",
+                src="nbextension",
                 dest="swanintro",
                 require="swanintro/extension"),
             ]
