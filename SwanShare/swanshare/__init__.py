578d258eb29ed5c6fda192c3f1fbc9792111b2a8
Diogo Castro <diogo.castro@cern.ch>
2020-09-02 16:47:14 +0200

    SwanShare: pip installable

--- SwanShare/swanshare/__init__.py
+++ SwanShare/swanshare/__init__.py
@@ -1,14 +1,16 @@
+from ._version import __version__ 
+
 
 def _jupyter_nbextension_paths():
     # Used by "jupyter nbextension" command to install frontend extension
     return [dict(
                 section="tree",
-                src="js",
+                src="nbextension",
                 dest="swanshare",
                 require="swanshare/extension"),
             dict(
                 section="notebook",
-                src="js",
+                src="nbextension",
                 dest="swanshare",
                 require="swanshare/extension"),
             ]
--- SwanShare/webpack.config.js
+++ SwanShare/webpack.config.js
@@ -1,18 +1,19 @@
 var path = require('path');
 
 module.exports = {
-    entry: './js/extension.js',
+    entry: './src/extension.js',
     output: {
-        path: path.resolve(__dirname, 'swanshare/js'),
+        path: path.resolve(__dirname, 'swanshare/nbextension'),
         filename: 'extension.js',
         libraryTarget: 'umd'
     },
     externals: ['jquery', 'base/js/namespace', 'base/js/utils', 'base/js/events', 'services/config', 'services/contents',
-        'require', 'base/js/dialog', 'base/js/keyboard', 'moment', 'services/config'],
+        'require', 'base/js/dialog', 'base/js/keyboard', 'moment', 'services/config'
+    ],
     module: {
         rules: [{
             test: /\.html$/,
-            use: [ {
+            use: [{
                 loader: 'html-loader',
                 options: {
                     minimize: true
