cd56ad43c78a165bbabc938ad08d69d051f2b9ed
Diogo Castro <diogo.castro@cern.ch>
2020-09-02 16:47:14 +0200

    SwanShare: pip installable

---
+++ SwanShare/README.md
@@ -0,0 +1,14 @@
+# SwanShare
+
+Jupyter extension to allow sharing integration between SWAN and CERNBox.
+
+This extension is composed of a Python package named `swanshare`, which installs the nbextension.
+It's not supposed to contain a JupyterLab because this functionality is being replaced by a different package.
+
+## Install
+
+```bash
+pip install swanshare
+jupyter nbextension install swanshare --py
+jupyter nbextension enable swanshare --py
+```
--- SwanShare/setup.py
+++ SwanShare/setup.py
@@ -1,13 +1,82 @@
-#!/usr/bin/env python
+"""
+Setup Module to setup Python Handlers for the SwanNotifications extension.
+"""
+import os
 
-from setuptools import setup, find_packages
+from jupyter_packaging import (
+    create_cmdclass, install_npm, ensure_targets,
+    combine_commands, ensure_python, get_version,
+)
+import setuptools
 
-setup(name='swanshare',
-      version='0.0.1',
-      description='Sharing for SWAN',
-      include_package_data=True,
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=[
-      ],
-  )
+name="swanshare"
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+ensure_python(">=3.5")
+
+# Get our version
+version = get_version(os.path.join(name, "_version.py"))
+
+nb_path = os.path.join(HERE, name, "nbextension")
+
+# Representative files that should exist after a successful build
+jstargets = [
+    os.path.join(nb_path, "extension.js"),
+]
+
+package_data_spec = {
+    name: [
+        "*"
+    ]
+}
+
+# data_files_spec = [
+#     ("share/jupyter/lab/extensions", lab_path, "*.tgz"),
+# ]
+
+cmdclass = create_cmdclass("jsdeps", 
+    package_data_spec=package_data_spec,
+    data_files_spec=[]
+)
+#data_files_spec=data_files_spec
+
+cmdclass["jsdeps"] = combine_commands(
+    install_npm(HERE, build_cmd="webpack"),
+    ensure_targets(jstargets),
+)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name=name,
+    version=version,
+    url="https://github.com/swan-cern/jupyter-extensions",
+    author="SWAN Admins",
+    description="Sharing for SWAN",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    cmdclass= cmdclass,
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    zip_safe=False,
+    include_package_data=True,
+    license="AGPL-3.0",
+    platforms="Linux, Mac OS X, Windows",
+    keywords=["Jupyter", "SWAN", "CERN"],
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Jupyter",
+    ],
+)
+
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_args)
