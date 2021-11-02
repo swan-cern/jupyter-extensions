624ce696c9eed9593136b26bc4b0e5286f7b4146
Omar Zapata <Omar.Zapata@cern.ch>
2021-11-02 15:51:31 +0100

    removed unneeded icons for SwanProjects

--- SwanProjects/src/icons.ts
+++ SwanProjects/src/icons.ts
@@ -4,26 +4,8 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 
 import swanProjectIconStr from '../style/project.svg';
-import swanReadmeIconStr from '../style/list-alt.svg';
-import cmsIconStr from '../style/cms.svg';
-import sftIconStr from '../style/sft.svg';
 
 export const swanProjectIcon = new LabIcon({
   name: 'jupyterlab_swan:project',
   svgstr: swanProjectIconStr,
-});
-
-export const swanReadmeIcon = new LabIcon({
-  name: 'jupyterlab_swan:reame',
-  svgstr: swanReadmeIconStr,
-});
-
-export const cmsIcon = new LabIcon({
-  name: 'jupyterlab_swan:cms',
-  svgstr: cmsIconStr,
-});
-
-export const sftIcon = new LabIcon({
-  name: 'jupyterlab_swan:sft',
-  svgstr: sftIconStr,
 });
