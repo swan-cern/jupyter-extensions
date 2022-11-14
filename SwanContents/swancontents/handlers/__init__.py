60cef672ee10c523cdf30728379c00dac77aa0aa
Krishnan R <krishnanr1997@gmail.com>
2022-11-14 19:06:12 +0100

    Update Copyright Message and make it dynamic

--- SwanContents/swancontents/handlers/__init__.py
+++ SwanContents/swancontents/handlers/__init__.py
@@ -7,6 +7,7 @@ from .share import ShareHandler
 from .tree import TreeHandler
 from .download import DownloadHandler
 from ..filemanager.handlers import FetchHandler, ContentsHandler
+import datetime
 
 def load_jupyter_server_extension(nb_server_app):
     """ Used as a server extension in order to install the new handlers """
@@ -22,6 +23,10 @@ def load_jupyter_server_extension(nb_server_app):
                     (r"/download", DownloadHandler)]
 
     web_app = nb_server_app.web_app
+
+    # Calculate current year for copyright message
+    nb_server_app.jinja_template_vars['current_year'] = datetime.datetime.now().year
+
     for handler in new_handlers:
         pattern = url_path_join(web_app.settings['base_url'], handler[0])
         new_handler = tuple([pattern] + list(handler[1:]))
