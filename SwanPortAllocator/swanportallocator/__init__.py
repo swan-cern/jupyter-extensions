7bd72a64ad6b7c2d0bb12e64ecb5cc5a050ce0b1
Diogo Castro <diogo.castro@cern.ch>
2023-10-16 19:53:56 +0200

    SwanPortAllocator: adapt to jupyter-server

--- SwanPortAllocator/swanportallocator/__init__.py
+++ SwanPortAllocator/swanportallocator/__init__.py
@@ -4,7 +4,7 @@ def _jupyter_nbextension_paths():
     # Empty to avoid error when automatically trying to enable all nbextensions
     return []
 
-def _jupyter_server_extension_paths():
+def _jupyter_server_extension_points():
     """Used by "jupyter serverextension" command to install web server extension'"""
     return [{
         "module": "swanportallocator.portallocator"
