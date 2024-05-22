ab03cda00522bf63f0a6c0d6737652e6d86f53c3
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2024-05-22 17:01:55 +0200

    SparkConnector: readd function to install extension for classic UI

--- SparkConnector/sparkconnector/__init__.py
+++ SparkConnector/sparkconnector/__init__.py
@@ -8,6 +8,14 @@ except ImportError:
     warnings.warn("Importing 'sparkconnector' outside a proper installation.")
     __version__ = "dev"
 
+def _jupyter_nbextension_paths():
+    """ Used by "jupyter nbextension" command to install frontend extension """
+    return [dict(
+                section="notebook",
+                src="nbextension",
+                dest="sparkconnector",
+                require="sparkconnector/extension"),
+            ]
 
 def _jupyter_labextension_paths():
     return [{
