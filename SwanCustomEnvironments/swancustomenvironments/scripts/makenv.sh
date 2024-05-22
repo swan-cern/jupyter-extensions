05e8b895f082553f405244a0ac9383c80f22ad50
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-05-22 11:51:19 +0200

    requirements not found error also prints current requirements

--- SwanCustomEnvironments/swancustomenvironments/scripts/makenv.sh
+++ SwanCustomEnvironments/swancustomenvironments/scripts/makenv.sh
@@ -132,7 +132,7 @@ elif [[ $REQUIREMENTS == http* ]]; then
         exit 1
     fi
 else
-    >&2 _log "ERROR: Requirements not found."
+    >&2 _log "ERROR: Requirements not found (${REQUIREMENTS})."
     exit 1
 fi
 
