6cb0750358f4d8aab9d96e18c00ac952fc856ed9
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-05-29 14:13:09 +0200

    improved docstring

--- SwanCustomEnvironments/swancustomenvironments/serverextension.py
+++ SwanCustomEnvironments/swancustomenvironments/serverextension.py
@@ -21,7 +21,12 @@ class SwanCustomEnvironments(Configurable):
 
 
 class SwanCustomEnvironmentsApiHandler(APIHandler):
-    """API handler for creating custom environments"""
+    """
+    API handler for creating custom environments
+    Runs a local script to create a custom environment and streams the output to the client
+    Environment name and repository URL are passed as query arguments and are mandatory
+    ACCPy version is optional (if not provided, generic python is used)
+    """
 
     def initialize(self):
         self.config = SwanCustomEnvironments(config=self.config)
