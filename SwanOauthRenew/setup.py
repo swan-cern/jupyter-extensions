c0a189aa92ba3f791b3a6faace4e049f14c48820
Diogo Castro <diogo.castro@cern.ch>
2024-06-17 21:40:14 +0200

    swanoauthrenew: automatic installation And make the extension compatible with jupyter server

--- SwanOauthRenew/setup.py
+++ SwanOauthRenew/setup.py
@@ -3,10 +3,20 @@ Setup Module to setup Python Handlers for the SwanNotebookViewer extension.
 """
 import os
 
-from jupyter_packaging import get_version
+from jupyter_packaging import get_version, create_cmdclass, ensure_targets
 import setuptools
 
 name="swanoauthrenew"
+
+data_files_spec = [(
+        "etc/jupyter/jupyter_server_config.d",
+        "jupyter_server_config.d",
+        "swanoauthrenew.json",
+    )]
+
+cmdclass = create_cmdclass('jsdeps', data_files_spec=data_files_spec)
+
+cmdclass["jsdeps"] = ensure_targets([])
 
 # Get our version
 version = get_version(os.path.join(name, "_version.py"))
@@ -22,9 +32,10 @@ setup_args = dict(
     description="Extension that renews an OAuth2 token",
     long_description= long_description,
     long_description_content_type="text/markdown",
+    cmdclass=cmdclass,
     packages=setuptools.find_packages(),
     install_requires=[
-        'notebook',
+        'jupyter_server',
         'requests',
         'PyJWT'
     ],
