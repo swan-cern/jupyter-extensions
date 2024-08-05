330e7c94a05b9751fabcafbb11ae24d8757183b9
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-08-05 12:15:24 +0200

    swancustomenvironments: change git url pattern and its comments
    Allow the git url pattern to accept `.git` extension and simplify comments' explanation

--- SwanCustomEnvironments/swancustomenvironments/scripts/makenv.sh
+++ SwanCustomEnvironments/swancustomenvironments/scripts/makenv.sh
@@ -77,10 +77,9 @@ done
 # --------------------------------------------------------------------------------------------
 # Validate input arguments
 
-# Git URL pattern: https://github.com/<username>/<repo_name>(/?) or https://gitlab.cern.ch/<username>/<repo_name>(/?)
-REPO_GIT_PATTERN='^https?:\/\/(github\.com|gitlab\.cern\.ch)\/([a-zA-Z0-9_-]+)\/([a-zA-Z0-9_-]+)\/?$'
-# EOS path pattern: $CERNBOX_HOME/<folder1>/<folder2>/...(/?) or /eos/user/<lowercase_first_letter>/<username>/<folder1>/<folder2>/...(/?)
-# Note: folders after username are optional
+# Git URL pattern: https://github.com/<username>/<repo_name>(/?) or https://gitlab.cern.ch/<username>/<repo_name>((/?)|(.git?))
+REPO_GIT_PATTERN='^https?:\/\/(github\.com|gitlab\.cern\.ch)\/([a-zA-Z0-9_-]+)\/([a-zA-Z0-9_-]+)(\/|\.git)?$'
+# EOS path pattern: $CERNBOX_HOME/<folder1>/<folder2>/...(/?) or /eos/user/<lowercase_first_letter>/<username>/<folder1>?/<folder2>?/...(/?)
 REPO_EOS_PATTERN='^(\$CERNBOX_HOME(\/[^<>|\\:()&;,\/]+)*\/?|\/eos\/user\/[a-z](\/[^<>|\\:()&;,\/]+)+\/?)$'
 
 # Checks if the provided Acc-Py version is valid
