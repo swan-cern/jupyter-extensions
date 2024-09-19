a05e456160df50f7f9cfb79a22bf16e851c2b968
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-09-19 11:31:39 +0200

    swancustomenvs: Use `echo` instead of `_log`
    `_log` is not defined yet. As we don't need to store these definitions in the log file, we can go with `echo`

--- SwanCustomEnvironments/swancustomenvironments/scripts/makenv.sh
+++ SwanCustomEnvironments/swancustomenvironments/scripts/makenv.sh
@@ -9,8 +9,8 @@
 CURRENT_ENV_NAME=$(find "/home/$USER" -type d -name "*_env" | head -n 1 | cut -d '/' -f4)
 CURRENT_REPO_PATH=$(tail -n 1 "/home/$USER/.bash_profile" | cut -d ' ' -f2)
 if [ -n "${CURRENT_ENV_NAME}" ]; then
-    _log "ENVIRONMENT_ALREADY_EXISTS:${CURRENT_ENV_NAME}"
-    _log "REPO_PATH:${CURRENT_REPO_PATH#$HOME}"
+    echo "ENVIRONMENT_ALREADY_EXISTS:${CURRENT_ENV_NAME}"
+    echo "REPO_PATH:${CURRENT_REPO_PATH#$HOME}"
     exit 1
 fi
 
