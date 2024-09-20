464a6bff8d32578a870fb04c99c86117479853b8
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-09-20 17:04:38 +0200

    swancustomenvs: Remove deprecated check of accpy version

--- SwanCustomEnvironments/swancustomenvironments/scripts/builders/accpy.sh
+++ SwanCustomEnvironments/swancustomenvironments/scripts/builders/accpy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Checks if the provided Acc-Py version is valid
-if [ ! -e "$ACCPY_PATH/base/$BUILDER_VERSION" ]; then
-    _error "Invalid Acc-Py version (${BUILDER_VERSION})."
-fi
-
 # Set up Acc-Py and create the environment
 source "${ACCPY_PATH}/base/${BUILDER_VERSION}/setup.sh"
 acc-py venv ${ENV_PATH} | tee -a ${LOG_FILE}
