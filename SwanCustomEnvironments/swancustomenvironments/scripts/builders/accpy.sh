f6f17b66395ba0271b2205091e4ef89870badbf4
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-11-13 15:40:13 +0100

    swancustomenvironments: Install missing SparkConnector dependency
    `requests` is also a SparkConnector dependency

--- SwanCustomEnvironments/swancustomenvironments/scripts/builders/accpy.sh
+++ SwanCustomEnvironments/swancustomenvironments/scripts/builders/accpy.sh
@@ -5,7 +5,7 @@ if [ -n "${INSTALL_NXCALS}" ]; then
     SPARKCONNECTOR="sparkconnector==$(python -c 'import sparkconnector; print(sparkconnector.__version__)')"
     SPARKMONITOR="sparkmonitor==$(python -c 'import sparkmonitor; print(sparkmonitor.__version__)')"
     NXCALS="nxcals"
-    SWANPORTALLOCATOR="swanportallocator" # TODO: Remove swanportallocator installation when the SparkConnector package gets properly updated
+    SPARKCONNECTOR_DEPENDENCIES="swanportallocator requests" # TODO: Remove swanportallocator and requests installation when the SparkConnector package gets properly updated
 fi
 
 # Set up Acc-Py and create the environment
@@ -19,11 +19,11 @@ eval "${ACTIVATE_ENV_CMD}"
 
 # Install packages in the environment and the same ipykernel that the Jupyter server uses
 _log "Installing packages from ${REQ_PATH}..."
-pip install -r "${REQ_PATH}" "ipykernel==${IPYKERNEL_VERSION}" ${NXCALS} ${SPARKMONITOR} ${SWANPORTALLOCATOR} 2>&1 | tee -a ${LOG_FILE} # TODO
+pip install -r "${REQ_PATH}" "ipykernel==${IPYKERNEL_VERSION}" ${NXCALS} ${SPARKMONITOR} ${SPARKCONNECTOR_DEPENDENCIES} 2>&1 | tee -a ${LOG_FILE} # TODO
 
 
 # -------------- HACK SECTION --------------
-# Install SWANPORTALLOCATOR separately, install SparkConnector without its dependencies and change the configuration file
+# Install SPARKCONNECTOR_DEPENDENCIES separately, install SparkConnector without its dependencies and change the configuration file
 # TODO: Remove this when the SparkConnector package gets properly updated
 if [ -n "${INSTALL_NXCALS}" ]; then
     pip install ${SPARKCONNECTOR} --no-deps 2>&1 | tee -a ${LOG_FILE}
