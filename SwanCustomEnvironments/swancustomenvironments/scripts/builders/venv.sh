380b3c2590dfad1010ad8d3cd43cfbe8357e236a
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-12-05 15:25:45 +0100

    swancustomenvironments: Remove swan middle layer
    Due to the fact `venv-pack` (used by nxcals) can only ship one environment (user's), the packages installed in the middle layer won't be shipped, which leads to discrepancies in the results. For that reason, we remove this middle layer and ignore the extra packages installed by swan, ensuring they stay updated and reliable.
    In spite of this, the requirements file must not be modified, which means a temporary and modified version of requirements.txt has to be created and used for the installation

--- SwanCustomEnvironments/swancustomenvironments/scripts/builders/venv.sh
+++ SwanCustomEnvironments/swancustomenvironments/scripts/builders/venv.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Create a middle layer for installing ipykernel, putting it apart from the user environment
-uv venv $SWAN_ENV --seed 2>&1
-source $SWAN_ENV/bin/activate
-uv pip install "ipykernel==${IPYKERNEL_VERSION}"
-SWAN_PACKAGES_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
-deactivate
-
 if [ "${RESOLVED_REQ}" = true ]; then
     uv venv ${ENV_PATH} --seed 2>&1
 else
@@ -21,7 +14,7 @@ eval "${ACTIVATE_ENV_CMD}"
 # Install user-requested packages in the environment.
 # Use uv for better performance if environment is fully resolved;
 # Otherwise, use pip for resolution (more reliable long-term).
-_log "Installing packages from ${REQ_PATH}..."
+_log "Installing packages from ${ORIGINAL_REQ_PATH}..."
 if [ "${RESOLVED_REQ}" = true ]; then
     uv pip install -r "${REQ_PATH}" ${IPYKERNEL} 2>&1
 else
@@ -30,8 +23,3 @@ fi
 if [ $? -ne 0 ]; then
     exit 1
 fi
-
-# Inject middle layer packages into the user environment by adding a .pth file to
-# the environment site-packages that contains the path to the middle layer site-packages
-USER_PACKAGES_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
-echo ${SWAN_PACKAGES_PATH} > ${USER_PACKAGES_PATH}/$(basename $SWAN_ENV).pth
