d10e9f67220bf91a1cbfa92f133d16c2862796f7
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-12-09 16:42:29 +0100

    swancustomenvironments: Remove swan middle layer
    Keep ipykernel package definition consistent with the remaining extra packages.
    Due to the fact `venv-pack` (used by nxcals) doesn't support editable packages to be shipped with the environment, and the swan middle layer was based on the use of a .pth file, which is seen as a source of editable packages and therefore makes venv-pack fail. For this reason, the swan middle layer has been removed and the extra packages have been added directly to the environment. In spite of this, the requirements file must not be modified.

--- SwanCustomEnvironments/swancustomenvironments/scripts/builders/accpy.sh
+++ SwanCustomEnvironments/swancustomenvironments/scripts/builders/accpy.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-# Create a middle layer for installing ipykernel, putting it apart from the user environment
-uv venv $SWAN_ENV --seed 2>&1
-source $SWAN_ENV/bin/activate
-uv pip install "ipykernel==${IPYKERNEL_VERSION}"
-SWAN_PACKAGES_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
-deactivate
-
 # If using NXCALS, we need to also install the Spark packages and their dependencies in the SWAN environment
 if [ -n "${USE_NXCALS}" ]; then
     SPARKCONNECTOR="sparkconnector==$(python -c 'import sparkconnector; print(sparkconnector.__version__)')"
     SPARKMONITOR="sparkmonitor==$(python -c 'import sparkmonitor; print(sparkmonitor.__version__)')"
     SPARKCONNECTOR_DEPENDENCIES="swanportallocator requests" # TODO: Remove swanportallocator and requests installation when the SparkConnector package gets properly updated
-    
-    # Activate the SWAN environment for installing the Spark packages
-    source $SWAN_ENV/bin/activate
-    uv pip install ${SPARKMONITOR} ${SPARKCONNECTOR_DEPENDENCIES} 2>&1
-
-    # -------------- HACK SECTION --------------
-    # Install SPARKCONNECTOR_DEPENDENCIES separately, install SparkConnector without its dependencies and change the configuration file
-    # TODO: Remove this when the SparkConnector package gets properly updated
-    uv pip install ${SPARKCONNECTOR} --no-deps 2>&1
-    wget https://raw.githubusercontent.com/swan-cern/jupyter-extensions/refs/heads/swan-on-tn/SparkConnector/sparkconnector/configuration.py -O ${SWAN_PACKAGES_PATH}/sparkconnector/configuration.py 2>&1
 fi
 
 # Set up Acc-Py and create the environment
@@ -41,15 +24,27 @@ if [ "${RESOLVED_REQ}" = true ]; then
     # Use the same pip configuration as the Acc-Py default pip
     ACCPY_PIP_CONF="-i $(pip config get global.index-url) --allow-insecure-host $(pip config get global.trusted-host)"
     uv pip install ${ACCPY_PIP_CONF} -r "${REQ_PATH}" 2>&1
+    # Enforce installation of our version of ipykernel and its dependencies
+    uv pip install ${ACCPY_PIP_CONF} ${IPYKERNEL} 2>&1
 else
     pip install -r "${REQ_PATH}" 2>&1
-fi
-if [ $? -ne 0 ]; then
-    exit 1
+    # Enforce installation of our version of ipykernel and its dependencies
+    pip install ${IPYKERNEL} 2>&1
 fi
 
-# Inject middle layer packages into the user environment by adding a .pth file to
-# the environment site-packages that contains the path to the middle layer site-packages
-USER_PACKAGES_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
-echo ${SWAN_PACKAGES_PATH} > ${USER_PACKAGES_PATH}/$(basename $SWAN_ENV).pth
+if [ -n "${USE_NXCALS}" ]; then
+    # For NXCALS, enforce installation of our Spark extensions and their dependencies at certain versions
+    if [ "${RESOLVED_REQ}" = true ]; then
+        uv pip install ${ACCPY_PIP_CONF} ${SPARKMONITOR} ${SPARKCONNECTOR_DEPENDENCIES} 2>&1
+        uv pip install ${ACCPY_PIP_CONF} ${SPARKCONNECTOR} --no-deps 2>&1
+    else
+        pip install ${SPARKMONITOR} ${SPARKCONNECTOR_DEPENDENCIES} 2>&1
+        pip install ${SPARKCONNECTOR} --no-deps 2>&1
+    fi
 
+    # -------------- HACK SECTION --------------
+    # Install SPARKCONNECTOR_DEPENDENCIES separately, install SparkConnector without its dependencies and change the configuration file
+    # TODO: Remove this when the SparkConnector package gets properly updated
+    USER_PACKAGES_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
+    wget https://raw.githubusercontent.com/swan-cern/jupyter-extensions/refs/heads/swan-on-tn/SparkConnector/sparkconnector/configuration.py -O ${USER_PACKAGES_PATH}/sparkconnector/configuration.py 2>&1
+fi
