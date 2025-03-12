a5247e8a4f5243f10e123b56d69b11d612282b18
Pedro Maximino <pedro.maximino@cern.ch>
2025-03-12 15:43:53 +0100

    swancontents: Remove save_notebook method
    Remove this method, as it was used when SWAN was running in the Puppet
    managed machines, to save notebooks

--- SwanContents/swancontents/filemanager/eos/fileio.py
+++ SwanContents/swancontents/filemanager/eos/fileio.py
@@ -107,58 +107,6 @@ class SwanFileManagerMixin(FileManagerMixin):
         else:
             return super()._get_os_path(path)
 
-    def _save_notebook(self, os_path, nb, capture_validation_error=None):
-        """
-        Save a notebook on EOS via FUSE with the default routine.
-        Plus, store a copy of the same notebook on the host machine via docker volume.
-        """
-        import time
-        def write_notebook_to_local(path, content, encoding='utf-8'):
-            try:
-                fout = io.open(path, 'w', encoding=encoding)
-                nbformat.write(content, fout, version=nbformat.NO_CONVERT)
-                fout.flush()
-                os.fsync(fout.fileno())
-                fout.close()
-            except:
-                pass
-            return
-
-        def read_notebook_from_local(path, encoding='utf-8', as_version=4):
-            try:
-                with io.open(path, 'r', encoding=encoding) as fin:
-                    return nbformat.read(fin, as_version=as_version)
-            except:
-                return nbformat.v4.new_notebook()
-
-        # If the path on the host is defined, save a copy of the notebook there
-        if ('USERDATA_PATH' in os.environ and os.path.isdir(os.environ['USERDATA_PATH'])):
-            # Define the filename for the local copy
-            dirname, basename = os.path.split(os_path)
-            local_fname = "-".join([os.environ['USER'], "nb", str(int(time.time())), dirname.replace(os.sep, "-"), basename])
-            local_path = os.path.join(os.environ['USERDATA_PATH'], local_fname)
-
-            # Write the notebook locally and check for consistency
-            local_retry = 10
-            write_notebook_to_local(local_path, nb)
-            for i in range(0, local_retry):
-                read_nb = read_notebook_from_local(local_path)
-                if (nb == read_nb):
-                    break
-                else:
-                    #time.sleep(0.1*2**i)    # Backoff on retry (100ms to 51.2s)
-                    time.sleep(0.5)
-                    write_notebook_to_local(local_path, nb)
-
-        # In all cases, save on eos via fuse
-        with self.atomic_writing(os_path, encoding="utf-8") as f:
-            nbformat.write(
-                nb,
-                f,
-                version=nbformat.NO_CONVERT,
-                capture_validation_error=capture_validation_error,
-            )
-
     @contextmanager
     def atomic_writing(self, os_path, *args, **kwargs):
         """Overload the default atomic_writing to use a different write method
