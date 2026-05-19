f180ca05886ad0c5a6976405e200cea58bec663c
Tomas Roun <tomas.roun8@gmail.com>
2026-05-19 16:25:36 +0200

    Fix is_repo check when rebuilding frontend assets
    This check is meant to skip rebuilding frontend assets
    when not inside a git repo but the check is incorrect.
    This causes e.g. `uv build` to not rebuild frontend assets
    if there are any local changes (CI is not affected).
    
    Since the extensions are inside a monorepo, we need to check
    for the .git folder one level up.

--- SparkConnector/setup.py
+++ SparkConnector/setup.py
@@ -51,7 +51,7 @@ js_command = combine_commands(
     ensure_targets(jstargets),
 )
 
-is_repo = os.path.exists(os.path.join(HERE, ".git"))
+is_repo = os.path.exists(os.path.join(HERE, "..", ".git"))
 if is_repo:
     cmdclass["jsdeps"] = js_command
 else:
