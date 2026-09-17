1e34bdf88c440499250d646c88df2ec31485a1a4
Cameron Lemon <180198309+cameron-lemon@users.noreply.github.com>
2026-09-17 14:14:18 +0200

    swancontents: Update super() syntax from Python 2 to 3 (UP008)

--- SwanContents/swancontents/swanclassic/notebookapp.py
+++ SwanContents/swancontents/swanclassic/notebookapp.py
@@ -53,14 +53,14 @@ class NotebookApp(ClassicNotebookApp):
         handlers.extend(load_handlers("swancontents.swanclassic.handlers.cernbox"))
         handlers.extend(load_handlers("swancontents.swanclassic.handlers.notebookviewer"))
         self.handlers.extend(handlers)
-        super(NotebookApp, self).initialize_handlers()
+        super().initialize_handlers()
 
     def initialize_settings(self):
         """
         Initialize settings to extend the jinja variables configuration
         in order to use them in our theme templates
         """
-        super(NotebookApp, self).initialize_settings()
+        super().initialize_settings()
         new_vars = (
             self.settings["jinja_template_vars"]
             if "jinja_template_vars" in self.settings
